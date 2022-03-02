93abe27012054aca92dada236f9940ffe392ae81
yoshlikmedia <yoshlikmedia@gmail.com>
2022-03-02 22:46:37 +0500

    Text updated

--- handlers/users/get_voice.py
+++ handlers/users/get_voice.py
@@ -7,11 +7,11 @@ from loader import dp
 @dp.message_handler(content_types=types.ContentType.VOICE)
 async def get_voice(msg: types.Message):
 
-    await msg.reply("Qabul qildim. Iltimos, kuting!.")
+    await msg.reply("<b>Qabul qildim. Iltimos, biroz kuting!</b>")
     with open('sample.flac', 'wb') as f:
         await msg.voice.download(f)
 
     pipe = pipeline("automatic-speech-recognition", "lucio/xls-r-uzbek-cv8")
     data = pipe("sample.flac")
-    await msg.delete()
-    await msg.answer(f"<b>Text:\n\n</b><code>{data['text'].capitalize()}</code>")
+
+    await msg.reply(f"<b>Text:\n\n</b><code>{data['text'].capitalize()}</code>")
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,4 +6,5 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}!")
+    await message.answer(f"<b>Assalom Alaykum, {message.from_user.full_name}!\n\n</b>"
+                         f"Botdan foydalanish uchun voice yuboring")
