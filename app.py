18b530589a8b0d55ca7bb5f77c55ad64516ca62f
yoshlikmedia <yoshlikmedia@gmail.com>
2022-03-02 22:29:30 +0500

    app.py config.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from aiogram import executor
+
+from loader import dp
+import middlewares, filters, handlers
+from utils.notify_admins import on_startup_notify
+from utils.set_bot_commands import set_default_commands
+
+
+async def on_startup(dispatcher):
+    await set_default_commands(dispatcher)
+
+    await on_startup_notify(dispatcher)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
+
