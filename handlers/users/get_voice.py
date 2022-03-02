161be729024e92300101dc21d809df71d2d2f2f3
yoshlikmedia <yoshlikmedia@gmail.com>
2022-03-02 22:42:19 +0500

    Update UX get_voice.py

--- handlers/users/get_voice.py
+++ handlers/users/get_voice.py
@@ -7,10 +7,11 @@ from loader import dp
 @dp.message_handler(content_types=types.ContentType.VOICE)
 async def get_voice(msg: types.Message):
 
+    await msg.reply("Qabul qildim. Iltimos, kuting!.")
     with open('sample.flac', 'wb') as f:
         await msg.voice.download(f)
 
     pipe = pipeline("automatic-speech-recognition", "lucio/xls-r-uzbek-cv8")
     data = pipe("sample.flac")
-
-    await msg.answer(f"<b>Text:\n\n</b><code>{data['text']}</code>")
+    await msg.delete()
+    await msg.answer(f"<b>Text:\n\n</b><code>{data['text'].capitalize()}</code>")
