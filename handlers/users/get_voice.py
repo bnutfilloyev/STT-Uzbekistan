f7a46643c1b2c6a38fbd7c9db67eba483d8d5ee8
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-06-22 07:41:37 +0500

    fix file name

--- handlers/users/get_voice.py
+++ handlers/users/get_voice.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from transformers import pipeline
+from uuid import uuid4
 
 from loader import dp
 
@@ -8,10 +9,11 @@ from loader import dp
 async def get_voice(msg: types.Message):
 
     await msg.reply("<b>Qabul qildim. Iltimos, biroz kuting!</b>")
-    with open('sample.flac', 'wb') as f:
+    file_name = str(uuid4()) + ".flac"
+    with open(file_name, 'wb') as f:
         await msg.voice.download(f)
 
     pipe = pipeline("automatic-speech-recognition", "lucio/xls-r-uzbek-cv8")
-    data = pipe("sample.flac")
+    data = pipe(file_name)
 
     await msg.reply(f"<b>Text:\n\n</b><code>{data['text'].capitalize()}</code>")
